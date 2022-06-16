e7b3a0a81d6cd049ce3a810fb1c7e556b36b1d04
chujesus <chujesus@gmail.com>
2022-06-16 16:55:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ $(".toggle").click(function (e) {
     $('.toggle').toggleClass('active');
     $('nav').toggleClass('active');
 });
+$(".home").click(function (e) {
+    e.preventDefault();
+    $('nav').removeClass('active');
+    $('.toggle').removeClass('active');
+    var valor= $(this).html();
+    $(location).attr('href',"#home");
+});
+$(".about-me").click(function (e) {
+    e.preventDefault();
+    $('nav').removeClass('active');
+    $('.toggle').removeClass('active');
+    var valor= $(this).html();
+    $(location).attr('href',"#about-me");
+});
+$(".footer").click(function (e) {
+    e.preventDefault();
+    $('nav').removeClass('active');
+    $('.toggle').removeClass('active');
+    var valor= $(this).html();
+    $(location).attr('href',"#footer");
+});
